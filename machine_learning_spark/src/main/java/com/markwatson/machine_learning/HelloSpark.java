c63df8b010f12fbef2948f48b907d71c7a9bbc57
Mark Watson <markw@markwatson.com>
2016-07-30 09:59:14 -0700

    Updated for version 2 of Spark machine learning library

--- machine_learning_spark/src/main/java/com/markwatson/machine_learning/HelloSpark.java
+++ machine_learning_spark/src/main/java/com/markwatson/machine_learning/HelloSpark.java
@@ -3,9 +3,11 @@ package com.markwatson.machine_learning;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -20,7 +22,13 @@ public class HelloSpark {
     JavaSparkContext sc = new JavaSparkContext("local", "Hello Spark");
 
     JavaRDD<String> lines = sc.textFile("data/test1.txt");
-    JavaRDD<String> tokens = lines.flatMap(line -> tokenize(line));
+    //JavaRDD<String> tokens = lines.flatMap(line -> tokenize(line)); // worked for mllib version 1.5, not for version 2.0
+    JavaRDD<String> tokens = lines.flatMap(new FlatMapFunction<String, String>() {
+      @Override
+      public Iterator<String> call(String s) {
+        return tokenize(s).iterator();
+      }
+    });
     JavaPairRDD<String, Integer> counts =
         tokens.mapToPair(
             token ->
